5e976eaed5afa7d8fed3e81fbf9910abe32e7424
Eric Clapton <ericclapton@Erics-Mac-mini.local>
2025-03-17 10:49:24 -0400

    Reset scope

--- main.tf
+++ main.tf
@@ -3,21 +3,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-# Variables defined in variables.tf
-variable "project_id" {
-  description = "The GCP project ID"
-  type        = string
-  default     = "optcl-lightweight-apps"
-}
-
-variable "region" {
-  description = "The GCP region for deployment"
-  type        = string
-  default     = "us-central1"
-}
-
-# Output the project ID for reference
-output "project_id" {
-  value = var.project_id
-}
--- variables.tf
+++ variables.tf
@@ -2,9 +2,11 @@
 variable "project_id" {
   description = "The GCP project ID"
   type        = string
+  default     = "optcl-lightweight-apps"
 }
 
 variable "region" {
   description = "The GCP region for deployment"
   type        = string
+  default     = "us-central1"
 }
