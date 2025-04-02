7c2e4b563964d7fa7379f32c18f9a4a53262fea5
Rory S <roryrs@gmail.com>
2025-04-02 08:21:24 -0400

    Some cleanup here

--- prod-hierarcy/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "project_id" { value = google_project.bjj_project.project_id }
-output "region" { value = var.region }
-output "vpc_names" { value = [for vpc in google_compute_network.vpc : vpc.name] }
--- prod-hierarcy/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "credentials_file" { type = string }
-variable "organization_id" { type = string }
-variable "billing_account_id" { type = string }
-variable "project_name" { type = string, default = "BJJ Game Builder 2025" }
-variable "project_id" { type = string, default = "bjj-game-builder-2025" }
-variable "region" { type = string, default = "us-central1" }
